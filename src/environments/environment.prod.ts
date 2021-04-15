651f0d1cddc6cc3504694a81e9e60973317e3573
ronaldoandriana <ronaldop16.aps1@gmail.com>
2021-04-15 18:43:41 +0400

    Set backend on heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: "http://localhost:8010"
+  apiUrl: "https://mbds-nodejs.herokuapp.com/api/"
+  //apiUrl: "http://localhost:8010/api/"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: "http://localhost:8010/api/"
+  apiUrl: "https://mbds-nodejs.herokuapp.com/api/"
+  //apiUrl: "http://localhost:8010/api/"
 };
 
 /*
